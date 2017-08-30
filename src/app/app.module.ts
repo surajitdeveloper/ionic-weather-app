461fb5c7851786bd6bc66ede64f6532ea938224f
Surajit Sadhukhan <surajit@localhost.localdomain>
2017-08-30 12:42:02 +0530

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,14 @@ import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
-import {  HttpModule } from "@angular/http"
+
 import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
-import { ViewPage } from "../pages/view/view";
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { LocationTrackerProvider } from '../providers/location-tracker/location-tracker';
 
 @NgModule({
   declarations: [
@@ -18,11 +17,9 @@ import { LocationTrackerProvider } from '../providers/location-tracker/location-
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
-    ViewPage
+    TabsPage
   ],
   imports: [
-    HttpModule,
     BrowserModule,
     IonicModule.forRoot(MyApp)
   ],
@@ -32,14 +29,12 @@ import { LocationTrackerProvider } from '../providers/location-tracker/location-
     AboutPage,
     ContactPage,
     HomePage,
-    TabsPage,
-    ViewPage
+    TabsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    LocationTrackerProvider
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,36 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-import { ViewPage } from "../view/view";
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-
   constructor(public navCtrl: NavController) {
 
   }
-  //lat: any;
-  //lon: any;
-  ionViewWillEnter()
-  {
-    if(navigator.geolocation)
-      {
-        navigator.geolocation.getCurrentPosition(function(position)
-        {
-          if(position.coords.latitude != null && position.coords.longitude != null)
-            {
-              localStorage.lat = position.coords.latitude;
-              localStorage.lon = position.coords.longitude;
-            }
-        });
-        this.navCtrl.push(ViewPage);
-      }
-      else
-      {
-        alert("No access");
-      }
-  }
-  
+
 }
